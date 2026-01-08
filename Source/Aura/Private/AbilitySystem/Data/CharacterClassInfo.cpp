251210bb4b4c67a61b852b19901d33f9738cb4a3
111crab <1553932758@qq.com>
2026-01-08 16:51:04 +0800

    Default Atrribute Effects

---
+++ Source/Aura/Private/AbilitySystem/Data/CharacterClassInfo.cpp
@@ -0,0 +1,10 @@
+// Copyright CRABOI
+
+
+#include "AbilitySystem/Data/CharacterClassInfo.h"
+
+FCharacterClassDefaultInfo UCharacterClassInfo::GetClassDefaultInfo(ECharacterClass CharacterClass)
+{
+	
+	return CharacterClassInformation.FindChecked(CharacterClass);
+}
